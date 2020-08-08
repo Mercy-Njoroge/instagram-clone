e1df7e810dbfc37b7c9441713545d491d53a3808
Mercy <aprilkasha42@gmail.com>
2020-08-08 12:56:43 +0300

    create home and user_profile views and pass them in urls.py file

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url('^$', views.welcome, name = 'welcome'),
-
+    url('^home/', views.home, name = 'home'),
+    url('^accounts/profile/', views.user_profile, name = 'user_profile'),
+    
 ]    
--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,40 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render (request, "welcome.html")
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    pics = Image.posted_pics()
+    comments = Comment.objects.all()
+    print(comments)
+
+    if request.method =='POST':
+        form = WelcomeMessageForm(request.POST) 
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = WelcomeMessageRecipient(name=name,email=email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('home')
+    else:
+        form = WelcomeMessageForm()
+
+    return render (request, 'all-photos/home.html',{"pics":pics,"letterForm":form, "comments":comments})
+
+
+def user_profile(request):  
+    current_user = request.user
+    if request.method =='POST':
+        form = EditProfile(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+
+            return redirect("home")
+    else:
+        form = EditProfile()
+    return render (request, 'all-photos/edit_profile.html',{"form":form})
