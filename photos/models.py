d00421ff035f8a5cc37b5319df7926d720cf1895
Mercy <aprilkasha42@gmail.com>
2020-08-08 14:09:38 +0300

    create first model,import models from django.db,create profile class tht inherits from model,create 3 variables tht rep diff columns in our db,use field objects to define the sort of data the db will store

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    pro_picture = models.ImageField(upload_to = 'profile-pics', null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_bio = HTMLField()
+
+
+    def __str__(self):
+        return self.user.username
